41541def035542d8a083a90e4e460d527a8812cb
Esin Adsoy <esin.adsoy@gmail.com>
2021-01-18 08:30:28 +0300

    hover

--- app/javascript/plugins/init_frame.js
+++ app/javascript/plugins/init_frame.js
@@ -1,12 +1,1 @@
-// const frameImage = () => {
-//   document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-//     anchor.addEventListener("click", function (e) {
-//       e.preventDefault();
-//       document.querySelector(this.getAttribute("href")).scrollIntoView({
-//         behavior: "smooth",
-//       });
-//     });
-//   });
-// };
-
-// export { frameImage };
+//
