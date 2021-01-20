474c17af50997624d46b5fd4e448ffb1f9c0d967
Esin Adsoy <esin.adsoy@gmail.com>
2021-01-20 09:34:23 +0300

    cleanup

--- app/javascript/plugins/init_cursor.js
+++ app/javascript/plugins/init_cursor.js
@@ -1,24 +1,24 @@
 const initCursor = () => {
   window.onmousemove = function (e) {
     if (e.target.classList.contains("follow-mouse")) {
-      //	Content of dot which is hovered
+      // Content of dot which is hovered
       const target = e.target.nextElementSibling;
 
-      //	Only work with hovered target
-      //	Add visible class hovered elem to distinguish
+      // Only work with hovered target
+      // Add visible class hovered elem to distinguish
       if (!target.classList.contains("visible")) {
         target.classList.add("visible");
       } else {
-        //	Get viewport offset of tooltip element
+        // Get viewport offset of tooltip element
         const offset = target.parentElement.getBoundingClientRect();
-        //	Tooltip distance from mouse(px)
+        // Tooltip distance from mouse(px)
         const tipDist = 15;
-        //	Calc and set new tooltip location
+        // Calc and set new tooltip location
         target.style.top = e.clientY - offset.top + tipDist + "px";
         target.style.left = e.clientX - offset.left + tipDist + "px";
       }
     } else {
-      //	Remove visible class
+      // Remove visible class
       const content = document.getElementsByClassName("content");
       for (let i = 0; i < content.length; i++) {
         content[i].classList.remove("visible");
